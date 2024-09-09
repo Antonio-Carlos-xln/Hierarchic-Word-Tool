b04140f92236ae68aab02d03901a4b0923105ced
Antonio Carlos <antoniocarlos.s.santana1999@gmail.com>
2024-09-09 03:41:07 -0300

    Create longInput.txt

--- app/src/test/java/com/antoniocarlos/hierarchicwordtool/MainIntegrationTest.java
+++ app/src/test/java/com/antoniocarlos/hierarchicwordtool/MainIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.antoniocarlos.hierarchydata;
+package com.antoniocarlos.hierarchicwordtool;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
